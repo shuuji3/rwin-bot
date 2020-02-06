31008e8fc95ec16bf26724ea15eec7ca6d69b000
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-02-06 13:23:30 +0900

    Install prettier.

--- index.js
+++ index.js
@@ -4,31 +4,28 @@ const puppeteer = require('puppeteer');
 
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
+const BASE_URL = 'https://r1.rwin.jp/CCSTsukuba';
 
 (async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
-    const browser = await puppeteer.launch({
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ]
-    });
+  // ログイン
+  const page = await browser.newPage();
+  await page.goto(BASE_URL);
+  await page.content();
+  await page.type('[name="txt_id"]', USERNAME);
+  await page.type('[name="txt_pass"]', PASSWORD + '\n');
 
-    // ログイン
-    const page = await browser.newPage();
-    await page.goto('https://r1.rwin.jp/CCSTsukuba/');
-    await page.content();
-    await page.type('[name="txt_id"]', USERNAME);
-    await page.type('[name="txt_pass"]', PASSWORD + '\n');
+  // 「施設単位」ページへ移動
+  await page.goto(`${BASE_URL}/ac_reservestateroom`);
+  // TODO: waitFor network request ends (very late response)
+  await page.waitFor('#ROOMLIST');
+  await page.select('#ROOMLIST', '21');
+  await page.waitFor('#TIME_TABLE1');
 
-    // 「施設単位」ページへ移動
-    await page.goto('https://r1.rwin.jp/CCSTsukuba/ac_reservestateroom');
-    await page.waitFor('#ROOMLIST');
-    await page.select('#ROOMLIST', '21');
-    await page.waitFor('#TIME_TABLE1');
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
-    await page.screenshot({path: 'screenshot.png', fullPage: true});
-
-    await browser.close();
-
+  await browser.close();
 })();
