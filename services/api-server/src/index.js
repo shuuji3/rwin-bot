e8fe79126b8962733993f5e0087589174c5b1a04
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-02-10 02:13:05 +0900

    Add /api/rooms endpoint and refactoring.

--- services/api-server/src/index.js
+++ services/api-server/src/index.js
@@ -4,18 +4,49 @@ const createConnection = require('./createConnection');
 
 (async () => {
   const knex = createConnection();
-  const schedules = await knex.select('*').from('schedules');
-
-  // Sqlite3ではDate型がUNIX時間の整数として保存されているため、Date型に戻す必要があります。
-  schedules.forEach(schedule => {
-    schedule.start = new Date(schedule.start);
-    schedule.end = new Date(schedule.end);
-  });
 
   const app = express();
   const port = 3000;
 
-  app.get('/api/schedules.json', (req, res) => res.json(schedules));
+  const endpoints = [
+    [
+      '/api/rooms',
+      () =>
+        knex
+          .distinct('roomTypeName', 'buildingName', 'roomName')
+          .from('schedules'),
+    ],
+    ['/api/schedules', () => knex.select('*').from('schedules')],
+  ];
+
+  for (const [path, schedules] of endpoints) {
+    await createEndpoint(app, path, schedules);
+  }
 
   app.listen(port, () => console.log(`Listening on port ${port} ...`));
 })();
+
+/**
+ * pathとscheduleを返す関数からエンドポイントを作成する。
+ * @param app
+ * @param {string} path
+ * @param {Function} queryCallback
+ * @returns {Promise<void>}
+ */
+async function createEndpoint(app, path, queryCallback) {
+  app.get(path, async (req, res) => {
+    console.log(`GET ${path}`);
+
+    const schedules = await queryCallback(req);
+
+    // Sqlite3ではDate型がUNIX時間の整数として保存されているため、Date型に戻す必要があります。
+    schedules.forEach(schedule => {
+      if (schedule.start != null && schedule.end != null) {
+        schedule.start = new Date(schedule.start);
+        schedule.end = new Date(schedule.end);
+      }
+    });
+
+    res.json(schedules);
+  });
+}
